62cbd724dc3d8708fb05e38ddc300e1cdc15d2c8
[NeeharikaNeti] <71735087+NeeharikaNeti@users.noreply.github.com>
2021-07-15 10:57:29 -0700

    Initial Commit

---
+++ Segues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Neeharika Neti on 7/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
